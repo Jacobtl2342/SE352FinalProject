0e0330a2d58565d359224f3423fa09b58a16d7da
Jacob Lopez <jacobtl2342@gmail.com>
2019-05-21 09:40:40 -0500

    MongoDB Intergration
    Added MongoDB and connected to cloud solution. When you run the server side of the application the test data will be displayed

--- server/src/main/java/com/server/DemoApplication.java
+++ server/src/main/java/com/server/DemoApplication.java
@@ -1,15 +1,26 @@
 package com.server;
 
-import org.springframework.boot.ApplicationRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import java.util.stream.Stream;
+
+
+import com.server.Repositories.UserRepository;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner{
+
+    @Autowired
+    private UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-}
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(userRepository.findAll());
+        System.out.println("Above this should appear the test data");
+    }
+} 
