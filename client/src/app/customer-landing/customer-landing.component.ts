c194423a7704e9b9b35b3f82f3e218784c2ae7df
Mutawi <mutawi@MacBook-Pro-2.local>
2019-05-31 14:17:51 -0500

    tabs in customer landing page

--- client/src/app/customer-landing/customer-landing.component.ts
+++ client/src/app/customer-landing/customer-landing.component.ts
@@ -7,8 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerLandingComponent implements OnInit {
 
-  constructor() { }
+  table = [
+    {ordernumber : 1, restaurant : "Gibsons", cost : 300, deliverystatus: false},
+    {ordernumber : 2, restaurant : "Tavern on Rush", cost : 400, deliverystatus: false},
+    {ordernumber : 3, restaurant : "Carmines", cost : 300, deliverystatus: false}   
+  ];
 
+  rtable = [
+    {restaurantnumber: 1, restaurantname : "Panera"},
+    {restaurantnumber: 2, restaurantname : "Shake Shack"},
+    {restaurantnumber: 3, restaurantname : "Wing Stop"},
+    {restaurantnumber: 4, restaurantname : "Burger King"},
+    {restaurantnumber: 5, restaurantname : "Taco Bell"},
+    {restaurantnumber: 6, restaurantname : "Pizza Hut"},
+    {restaurantnumber: 7, restaurantname : "Five Guys"},
+    {restaurantnumber: 8, restaurantname : "Subway"},
+    {restaurantnumber: 9, restaurantname : "McDonalds"}
+  ];
+
+  constructor() { }
+ 
   ngOnInit() {
   }
 
