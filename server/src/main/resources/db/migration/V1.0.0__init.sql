ab90718d9f463ed51e971f7b433da8ef05da9b57
Philip DeLeon <philip121@gmail.com>
2019-06-02 17:25:21 -0500

    Update V1.0.0__init.sql
    minor updates to h2 db table creations

--- server/src/main/resources/db/migration/V1.0.0__init.sql
+++ server/src/main/resources/db/migration/V1.0.0__init.sql
@@ -1,13 +1,13 @@
-CREATE TABLE RESTAURANT(id integer PRIMARY KEY, restaurant_name text, menu_id integer, admin_id integer);
+CREATE TABLE RESTAURANT(id identity not null PRIMARY KEY, restaurant_name text, menu_id integer, admin_id integer);
 
-CREATE TABLE MENU(id integer PRIMARY KEY, menu_name text);
+CREATE TABLE MENU(id identity not null PRIMARY KEY, menu_name text);
 
-CREATE TABLE MENU_ITEMS(id integer PRIMARY KEY, item_name text, menu_id_id integer, price float);
+CREATE TABLE MENU_ITEMS(id identity not null PRIMARY KEY, item_name text, menu_id_id integer, price float);
 
-CREATE TABLE ORDER_HISTORY(id integer PRIMARY KEY, payment_id integer, total_price float);
+CREATE TABLE ORDER_HISTORY(id identity not null PRIMARY KEY, payment_id integer, total_price float);
 
-CREATE TABLE ORDER_ITEMS(id integer PRIMARY KEY, order_id integer, menu_item_id integer, quantity integer);
+CREATE TABLE ORDER_ITEMS(id identity not null PRIMARY KEY, order_id integer, menu_item_id integer, quantity integer);
 
-CREATE TABLE PAYMENT(id integer PRIMARY KEY, payment_status text, method text);
+CREATE TABLE PAYMENT(id identity not null PRIMARY KEY, payment_status text, method text);
 
-CREATE TABLE RESTAURANT_ADMIN(id integer PRIMARY KEY, restaurant_id integer);
+CREATE TABLE RESTAURANT_ADMIN(id identity not null PRIMARY KEY, restaurant_id integer);
