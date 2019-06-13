b7e4d81e7d906562f24dcf56ac9479855d35dcd1
Jacob Lopez <jacobtl2342@gmail.com>
2019-06-13 15:38:30 -0500

    Driver
    updates to driver page with test data

--- client/src/app/driver-landing/driver-landing.component.ts
+++ client/src/app/driver-landing/driver-landing.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface avaliableOrders {
+  restaurantName: string;
+  orderNumber: number;
+  orderValue: number;
+}
+
+const ELEMENT_DATA: avaliableOrders[] = [
+  {orderNumber: 1, restaurantName: 'Panera', orderValue:25},
+  {orderNumber: 2, restaurantName: 'Shake Shack', orderValue: 35},
+  {orderNumber: 3, restaurantName: 'Wing Stop', orderValue: 32},
+  {orderNumber: 4, restaurantName: 'Burger King', orderValue: 55},
+  {orderNumber: 5, restaurantName: 'Taco Bell', orderValue: 47}
+];
+
 @Component({
   selector: 'app-driver-landing',
   templateUrl: './driver-landing.component.html',
@@ -8,7 +22,8 @@ import { Component, OnInit } from '@angular/core';
 export class DriverLandingComponent implements OnInit {
 
   constructor() { }
-
+  displayedColumns: string[] = ['orderNumber', 'restaurantName', 'orderValue'];
+  dataSource = ELEMENT_DATA;
   ngOnInit() {
   }
 
